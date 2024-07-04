5821de67c2d1503c35480f215929f00ff4a2ee66
DrCoder3 <guillencesar871@gmail.com>
2024-07-04 13:46:45 +0200

    added basic running animations

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,24 +1,27 @@
 extends CharacterBody2D
 
-
 const SPEED = 200.0
 
 func _physics_process(_delta):
+	var direction = Vector2()
+	direction.x = Input.get_axis("ui_left", "ui_right")
+	direction.y = Input.get_axis("ui_up", "ui_down")
 	
-	var direction_x = Input.get_axis("ui_left", "ui_right")
-	var direction_y = Input.get_axis("ui_up" , "ui_down")
-	
-	if direction_x and direction_y:
-		velocity.x = direction_x * SPEED/1.5
-		velocity.y = direction_y * SPEED/1.5
+	if direction.x < 0: 
+		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.play("run_loop")
+	elif direction.x > 0: 
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.play("run_loop")
+	elif direction.y:
+		$AnimatedSprite2D.play("run_loop")
 	else:
-		if direction_x:
-			velocity.x = direction_x * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-		if direction_y:
-			velocity.y = direction_y * SPEED
-		else:
-			velocity.y = move_toward(velocity.y, 0, SPEED)
+		$AnimatedSprite2D.play("idle")
+		
+	
+	if direction.length() > 0:
+		direction = direction.normalized()
+		
+	velocity = direction * SPEED
 	
 	move_and_slide()
