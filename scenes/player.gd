403bfa2d89457194e094dda9800f5eba951c215b
DrCoder3 <guillencesar871@gmail.com>
2024-07-08 17:47:52 +0200

    added a facing away idle animation

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,22 +1,31 @@
 extends CharacterBody2D
 
 const SPEED = 150.0
-
+var looking_up = false	
 func _physics_process(_delta):
 	var direction = Vector2()
 	direction.x = Input.get_axis("ui_left", "ui_right")
 	direction.y = Input.get_axis("ui_up", "ui_down")
-	
+
 	if direction.x < 0: 
+		looking_up = false
 		$AnimatedSprite2D.flip_h = true
 		$AnimatedSprite2D.play("run_loop")
 	elif direction.x > 0: 
+		looking_up = false
 		$AnimatedSprite2D.flip_h = false
 		$AnimatedSprite2D.play("run_loop")
-	elif direction.y:
+	elif direction.y < 0:
+		looking_up = true
+		$AnimatedSprite2D.play("run_loop")
+	elif direction.y > 0:
+		looking_up = false
 		$AnimatedSprite2D.play("run_loop")
 	else:
-		$AnimatedSprite2D.play("idle")
+		if looking_up:
+			$AnimatedSprite2D.play("idle_back")
+		else:
+			$AnimatedSprite2D.play("idle")
 		
 	
 	if direction.length() > 0:
