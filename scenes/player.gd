85398c7944abc27935b403995189ec524205d00c
DrCoder3 <guillencesar871@gmail.com>
2024-07-10 19:32:28 +0200

    added running to front animation

--- scenes/player.gd
+++ scenes/player.gd
@@ -4,7 +4,8 @@ const SPEED = 150.0
 
 @onready var anim = $AnimationPlayer
 func _physics_process(_delta):
-	var direction : Vector2 = Input.get_vector("ui_left","ui_right", "ui_up","ui_down").normalized() 
+	var direction : Vector2 = Input.get_vector("ui_left","ui_right", "ui_up","ui_down").normalized()
+	anim.play("run_loop") 
 	if direction:	
 		velocity = direction * SPEED
 	else:
