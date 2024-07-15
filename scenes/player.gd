16fcf2a22a688f2ae37a08fb2876c3d9fc14cd91
DrCoder3 <guillencesar871@gmail.com>
2024-07-15 16:46:11 +0200

    finished with basic running animations in animation tree

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,12 +2,17 @@ extends CharacterBody2D
 
 const SPEED = 150.0
 
-@onready var anim = $AnimationPlayer
+@onready var anim_tree : AnimationTree = $AnimationTree
+var direction : Vector2
 
+func _ready():
+	anim_tree.active = true 
+
+func _process(delta):
+	update_animation_var()
 
 func _physics_process(_delta):
-	var direction : Vector2 = Input.get_vector("ui_left","ui_right", "ui_up","ui_down").normalized()
-	anim.play("run_angle") 
+	direction  = Input.get_vector("ui_left","ui_right", "ui_up","ui_down").normalized()
 	if direction:	
 		velocity = direction * SPEED
 		if direction.x < 0:
@@ -18,4 +23,11 @@ func _physics_process(_delta):
 		velocity = Vector2.ZERO
 	move_and_slide()
 
+
+func update_animation_var():
+	anim_tree.set("parameters/conditions/idle", velocity == Vector2.ZERO)
+	anim_tree.set("parameters/conditions/run", velocity != Vector2.ZERO)
+	if direction != Vector2.ZERO:
+		anim_tree["parameters/idle/blend_position"] = direction.y
+		anim_tree["parameters/run/blend_position"] = direction
 
